3c4735446c669e2bdfa8f921b70a59f75517448b
Carlos Icaro <carlos.paiva@sou.inteli.edu.br>
2025-05-25 22:34:03 -0300

    feat: EndPoints implementados, testados e funcionais

---
+++ config/database.js
@@ -0,0 +1,16 @@
+// config/database.js
+require('dotenv').config();
+
+const { Pool } = require('pg');
+
+// Criando a pool de cnexões
+const pool = new Pool({
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_DATABASE,
+  password: process.env.DB_PASSWORD,
+  port: process.env.DB_PORT,
+  ssl: { rejectUnauthorized: false }
+});
+
+module.exports = pool;
